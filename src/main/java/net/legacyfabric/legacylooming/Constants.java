2de74b044c56d698ea5abc0b395074cbe34aeb4a
Cat Core <arthur.koalas@free.fr>
2023-04-30 00:51:53 +0200

    add intermediary version to manifest instead of mod json

--- src/main/java/net/legacyfabric/legacylooming/Constants.java
+++ src/main/java/net/legacyfabric/legacylooming/Constants.java
@@ -2,7 +2,7 @@ package net.legacyfabric.legacylooming;
 
 public class Constants {
     public static final String MAVEN = "https://repo.legacyfabric.net/repository/legacyfabric/";
-    public static final String INTERMEDIARY_PROPERTY = "legacyfabric:intermediary";
+    public static final String INTERMEDIARY_PROPERTY = "Legacy-Fabric-Intermediary-Version";
 
     public static String getIntermediaryURL(int version) {
         return MAVEN + "net/legacyfabric/" + switch (version) {
--- src/main/java/net/legacyfabric/legacylooming/LegacyLoomingGradlePlugin.java
+++ src/main/java/net/legacyfabric/legacylooming/LegacyLoomingGradlePlugin.java
@@ -1,18 +1,19 @@
 package net.legacyfabric.legacylooming;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.gson.JsonObject;
-import net.fabricmc.loom.LoomGradlePlugin;
 import net.fabricmc.loom.api.LoomGradleExtensionAPI;
+import net.fabricmc.loom.task.AbstractRemapJarTask;
+import net.fabricmc.loom.util.ZipUtils;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.PluginAware;
-import org.gradle.language.jvm.tasks.ProcessResources;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.jar.Manifest;
+
+import static net.fabricmc.loom.task.AbstractRemapJarTask.MANIFEST_PATH;
 
 public class LegacyLoomingGradlePlugin implements Plugin<PluginAware> {
     public static final String VERSION = Objects.requireNonNullElse(LegacyLoomingGradlePlugin.class.getPackage().getImplementationVersion(), "0.0.0+unknown");
@@ -32,31 +33,22 @@ public class LegacyLoomingGradlePlugin implements Plugin<PluginAware> {
             project.getExtensions().create("legacy", LegacyUtilsExtension.class, project);
             project.getExtensions().create("legacyFabricApi", LegacyFabricApiExtension.class, project);
 
-            project.getTasks().withType(ProcessResources.class).configureEach(processResources -> {
-                processResources.doLast(task -> {
-                    if (processResources.getOutputs().getFiles().getFiles().stream().anyMatch(file -> file.getName().equals("fabric.mod.json"))) {
-                        processResources.getOutputs().getFiles().getFiles()
-                                .stream().filter(file -> file.getName().equals("fabric.mod.json")).forEach(file -> {
-                                    try {
-                                        var object = LoomGradlePlugin.GSON.fromJson(new InputStreamReader(new FileInputStream(file)), JsonObject.class);
+            project.getTasks().configureEach(task -> {
+                if (task instanceof AbstractRemapJarTask remapJarTask) {
+                    remapJarTask.doLast(task1 -> {
+                        try {
+                            ZipUtils.transform(remapJarTask.getArchiveFile().get().getAsFile().toPath(), Map.of(MANIFEST_PATH, bytes -> {
+                                var manifest = new Manifest(new ByteArrayInputStream(bytes));
 
-                                        if (!object.has("custom")) {
-                                            object.add("custom", new JsonObject());
-                                        }
-
-                                        object.getAsJsonObject("custom")
-                                                .addProperty(Constants.INTERMEDIARY_PROPERTY, extension.getIntermediaryVersion().get());
-
-                                        var bytes = LoomGradlePlugin.GSON.toJson(object, JsonObject.class).getBytes(StandardCharsets.UTF_8);
-                                        var writer = new OutputStreamWriter(new FileOutputStream(file));
-                                        writer.write(bytes.toString());
-                                        writer.close();
-                                    } catch (IOException e) {
-                                        throw new RuntimeException(e);
-                                    }
-                                });
-                    }
-                });
+                                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                                manifest.write(out);
+                                return out.toByteArray();
+                            }));
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                }
             });
         }
     }
