ec0eef65db4a46fd4f37e2c3a96be509d4300f64
Cat Core <arthur.koalas@free.fr>
2023-04-29 23:17:14 +0200

    log Legacy Looming version

--- src/main/java/net/legacyfabric/legacylooming/LegacyLoomingGradlePlugin.java
+++ src/main/java/net/legacyfabric/legacylooming/LegacyLoomingGradlePlugin.java
@@ -8,8 +8,10 @@ import org.gradle.api.plugins.PluginAware;
 import org.gradle.language.jvm.tasks.ProcessResources;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class LegacyLoomingGradlePlugin implements Plugin<PluginAware> {
+    public static final String VERSION = Objects.requireNonNullElse(LegacyLoomingGradlePlugin.class.getPackage().getImplementationVersion(), "0.0.0+unknown");
     @Override
     public void apply(PluginAware target) {
         target.getPlugins().apply(LegacyRepositoryHandler.class);
@@ -17,6 +19,8 @@ public class LegacyLoomingGradlePlugin implements Plugin<PluginAware> {
         target.apply(ImmutableMap.of("plugin", "fabric-loom"));
 
         if (target instanceof Project project) {
+            project.getLogger().lifecycle("Legacy Looming: " + VERSION);
+
             var extension = project.getExtensions().create(LegacyLoomingExtensionAPI.class, "legacyLooming", LegacyLoomingExtensionImpl.class, project);
             project.getExtensions().getByType(LoomGradleExtensionAPI.class).getIntermediaryUrl()
                     .set(Constants.getIntermediaryURL(extension.getIntermediaryVersion().get()));
