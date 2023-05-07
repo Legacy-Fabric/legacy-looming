ade9bd4347b9967b8edc02420e5e3ce3e02aa06d
Cat Core <34719527+arthurbambou@users.noreply.github.com>
2023-05-07 12:30:00 +0200

    Update Constants.java

--- src/main/java/net/legacyfabric/legacylooming/Constants.java
+++ src/main/java/net/legacyfabric/legacylooming/Constants.java
@@ -1,15 +1,18 @@
 package net.legacyfabric.legacylooming;
 
 public class Constants {
-    public static final String MAVEN = "https://repo.legacyfabric.net/repository/legacyfabric/";
+    public static final String MAVEN = "https://maven.legacyfabric.net/";
     public static final String INTERMEDIARY_PROPERTY = "Legacy-Fabric-Intermediary-Version";
     public static final String VERSION_PROPERTY = "Legacy-Looming-Version";
 
     public static String getIntermediaryURL(int version) {
-        return MAVEN + "net/legacyfabric/" + switch (version) {
-            case 2 -> "v2/intermediary";
-            default -> "intermediary";
-        } + "/%1$s/intermediary-%1$s-v2.jar";
+        var url = MAVEN + "net/legacyfabric/" + switch (version) {
+            case 2 -> "v2/";
+            default -> "";
+        } + "intermediary/%1$s/intermediary-%1$s-v2.jar";
+        System.out.println(version);
+        System.out.println(url);
+        return url;
     }
 
     public static String getYarnGroup(int version) {
