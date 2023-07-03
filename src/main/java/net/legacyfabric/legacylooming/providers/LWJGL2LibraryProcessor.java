1a4d0c3964c1f6780ff62a2af27440789fa3cd33
Cat Core <arthur.koalas@free.fr>
2023-07-03 17:39:32 +0200

    change strategy

--- src/main/java/net/legacyfabric/legacylooming/providers/LWJGL2LibraryProcessor.java
+++ src/main/java/net/legacyfabric/legacylooming/providers/LWJGL2LibraryProcessor.java
@@ -4,6 +4,7 @@ import net.fabricmc.loom.configuration.providers.minecraft.library.Library;
 import net.fabricmc.loom.configuration.providers.minecraft.library.LibraryContext;
 import net.fabricmc.loom.configuration.providers.minecraft.library.LibraryProcessor;
 import net.fabricmc.loom.util.Platform;
+import net.legacyfabric.legacylooming.OperatingSystem;
 import org.gradle.api.artifacts.dsl.RepositoryHandler;
 
 import java.util.function.Consumer;
@@ -11,6 +12,7 @@ import java.util.function.Predicate;
 
 public class LWJGL2LibraryProcessor extends LibraryProcessor {
     public static final String VERSION = "2.9.4+legacyfabric.5";
+    private static boolean applied = false;
     public LWJGL2LibraryProcessor(Platform platform, LibraryContext context) {
         super(platform, context);
     }
@@ -28,15 +30,20 @@ public class LWJGL2LibraryProcessor extends LibraryProcessor {
     @Override
     public Predicate<Library> apply(Consumer<Library> dependencyConsumer) {
         return library -> {
-            if (library.group().equals("org.lwjgl.lwjgl")) {
-                if (library.name().equals("lwjgl-platform") && library.classifier() == null) return true;
-                final Library upgradedLibrary = library.withVersion(VERSION);
-                dependencyConsumer.accept(upgradedLibrary);
+            if (!applied) {
+                applied = true;
+                final Library[] libs = new Library[]{
+                        Library.fromMaven("org.lwjgl.lwjgl:lwjgl-platform:" + VERSION + ":" + getNativeClassifier(), Library.Target.NATIVES),
+                        Library.fromMaven("org.lwjgl.lwjgl:lwjgl_util:" + VERSION, Library.Target.RUNTIME),
+                        Library.fromMaven("org.lwjgl.lwjgl:lwjgl:" + VERSION, Library.Target.RUNTIME)
+                };
 
-                return false;
+                for (Library lib : libs) {
+                    dependencyConsumer.accept(lib);
+                }
             }
 
-            return true;
+            return !library.group().equals("org.lwjgl.lwjgl") || !library.name().equals("lwjgl-platform");
         };
     }
 
@@ -48,5 +55,19 @@ public class LWJGL2LibraryProcessor extends LibraryProcessor {
                 filter.includeGroup("org.lwjgl.lwjgl");
             });
         });
+    }
+
+    public static String getNativeClassifier() {
+        switch (OperatingSystem.CURRENT_OS) {
+            case macos -> {
+                return "natives-osx";
+            }
+            case linux, freebsd, openbsd -> {
+                return "natives-linux";
+            }
+            default -> {
+                return "natives-windows";
+            }
+        }
     }
 }
