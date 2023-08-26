c8da99e2b2c791b6b6eb369f685b99b849b492cb
Grayray75 <69988482+Grayray75@users.noreply.github.com>
2023-08-26 23:05:55 +0200

    Add check for v2 yarn mappings

--- src/main/java/net/legacyfabric/legacylooming/tasks/MigrateLegacyMappingsTask.java
+++ src/main/java/net/legacyfabric/legacylooming/tasks/MigrateLegacyMappingsTask.java
@@ -4,55 +4,69 @@ import net.fabricmc.loom.configuration.DependencyInfo;
 import net.fabricmc.loom.task.MigrateMappingsTask;
 import net.fabricmc.loom.util.Constants;
 import org.gradle.api.Project;
+import org.gradle.api.logging.Logger;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.options.Option;
 import org.gradle.work.DisableCachingByDefault;
 
 @DisableCachingByDefault(because = "Always rerun this tasks.")
 public abstract class MigrateLegacyMappingsTask extends MigrateMappingsTask {
-    private String mappings;
+    private Logger logger;
+    private String targetMappings;
 
     @Override
     @Option(option = "mappings", description = "Target mappings")
     public void setMappings(String mappings) {
         super.setMappings(mappings);
-        this.mappings = mappings;
+        this.targetMappings = mappings;
     }
 
     @Override
     @TaskAction
     public void doTask() throws Throwable {
         Project project = getProject();
+        logger = project.getLogger();
 
-        DependencyInfo minecraftDep = DependencyInfo.create(project, Constants.Configurations.MINECRAFT);
-        String minecraftDepString = minecraftDep.getDepString();
-        String minecraftVersion = minecraftDepString.substring(minecraftDepString.lastIndexOf(':') + 1);
+        try {
+            DependencyInfo minecraftDep = DependencyInfo.create(project, Constants.Configurations.MINECRAFT);
+            String minecraftDepString = minecraftDep.getDepString();
+            String minecraftVersion = minecraftDepString.substring(minecraftDepString.lastIndexOf(':') + 1);
 
-        DependencyInfo mappingsDep = DependencyInfo.create(project, Constants.Configurations.MAPPINGS);
-        String mappingsDepString = mappingsDep.getDepString();
+            DependencyInfo mappingsDep = DependencyInfo.create(project, Constants.Configurations.MAPPINGS);
+            String mappingsDepString = mappingsDep.getDepString();
 
-        this.checkForLegacyYarn(project, mappingsDepString, minecraftVersion);
+            this.checkForLegacyYarn(mappingsDepString, minecraftVersion);
+        } catch (Exception e) {
+            logger.warn("Failed to check if legacyfabric yarn mappings are used!", e);
+        }
 
         super.doTask();
     }
 
-    private void checkForLegacyYarn(Project project, String mappingsDepString, String minecraftVersion) {
+    private void checkForLegacyYarn(String mappingsDepString, String minecraftVersion) {
         if (mappingsDepString.startsWith("net.legacyfabric:yarn")) {
-            project.getLogger().info("Detected legacyfabric yarn mappings, adjusting target mappings");
-
-            String yarnBuild;
-            if (mappings.matches("^[0-9.]+\\+build\\.[0-9]+$")) {
-                yarnBuild = mappings;
-            }
-            else if (mappings.matches("^[0-9]+$")) {
-                yarnBuild = minecraftVersion + "+build." + mappings;
-            }
-            else {
-                project.getLogger().info("The provided target mappings aren't a yarn build, skipping");
-                return;
-            }
-
-            setMappings("net.legacyfabric:yarn:" + yarnBuild + ":v2");
+            logger.info("Detected legacyfabric yarn mappings, adjusting target mappings");
+            this.adjustTargetMappings("net.legacyfabric:yarn", minecraftVersion);
         }
+        else if (mappingsDepString.startsWith("net.legacyfabric.v2:yarn")) {
+            logger.info("Detected legacyfabric yarn v2 mappings, adjusting target mappings");
+            this.adjustTargetMappings("net.legacyfabric.v2:yarn", minecraftVersion);
+        }
+    }
+
+    private void adjustTargetMappings(String yarnPath, String minecraftVersion) {
+        String yarnBuild;
+        if (targetMappings.matches("^[0-9.]+\\+build\\.[0-9]+$")) {
+            yarnBuild = targetMappings;
+        }
+        else if (targetMappings.matches("^[0-9]+$")) {
+            yarnBuild = minecraftVersion + "+build." + targetMappings;
+        }
+        else {
+            logger.info("The provided target mappings aren't a yarn build, skipping");
+            return;
+        }
+
+        this.setMappings(yarnPath + ":" + yarnBuild + ":v2");
     }
 }
