b6751236a7169b59a9500c6539cb96c5129a6531
Cat Core <arthur.koalas@free.fr>
2025-09-27 14:34:09 +0200

    Update LWJGL to build 15

--- src/main/java/net/legacyfabric/legacylooming/providers/LWJGL2LibraryProcessor.java
+++ src/main/java/net/legacyfabric/legacylooming/providers/LWJGL2LibraryProcessor.java
@@ -11,7 +11,7 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class LWJGL2LibraryProcessor extends LibraryProcessor {
-    public static final String VERSION = "2.9.4+legacyfabric.14";
+    public static final String VERSION = "2.9.4+legacyfabric.15";
     private boolean applied = false;
     public LWJGL2LibraryProcessor(Platform platform, LibraryContext context) {
         super(platform, context);
